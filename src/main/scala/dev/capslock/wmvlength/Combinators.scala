9d1b2758c87089a082dff36861cb702b7a7425aa
Windymelt <windymelt@3qe.us>
2025-05-30 02:25:04 +0900

    BAD ENGLISH

--- src/main/scala/dev/capslock/wmvlength/Combinators.scala
+++ src/main/scala/dev/capslock/wmvlength/Combinators.scala
@@ -1,10 +1,11 @@
 package dev.capslock.wmvlength
 
+import scodec.Decoder
 import scodec.codecs.uint32L
 
 object Combinators {
-  val uint64LDecoder = for
-    l <- uint32L // due to little endian, lower bytes are read fast
+  val uint64LDecoder: Decoder[BigInt] = for
+    l <- uint32L // due to little endian, lower bytes are read first
     h <- uint32L
   yield (BigInt(h) << 32) | BigInt(l)
 }
